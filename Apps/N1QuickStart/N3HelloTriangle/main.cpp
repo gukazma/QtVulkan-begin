c647c9ea71dccbcdd2925c86c6bedbe81bab6c18
gukazma <gukazma@proton.me>
2023-08-13 01:12:43 +0800

    ✨ feat: hello triangle

--- Apps/N1QuickStart/N3HelloTriangle/main.cpp
+++ Apps/N1QuickStart/N3HelloTriangle/main.cpp
@@ -1,16 +1,24 @@
-#include <gtest/gtest.h>
+#include "TriangleRenderer.h"
 #include <QGuiApplication>
 #include <QLoggingCategory>
 #include <QVulkanInstance>
 #include <vulkan/vulkan.hpp>
 
-TEST(N1QuickStart, E3HelloTriangle)
-{
-    int                  argc = 1;
-    std::array<char*, 1> argv = {"E3HelloTriangle"};
-    QGuiApplication      app(argc, argv.data());
+VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
 
-    static vk::DynamicLoader dynamicLoader;
+Q_LOGGING_CATEGORY(lcVk, "qt.vulkan")
+
+class VulkanWindow : public QVulkanWindow
+{
+public:
+    QVulkanWindowRenderer* createRenderer() override { return new TriangleRenderer(this); }
+};
+
+int main(int argc, char* argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    static vk::DynamicLoader  dynamicLoader;
     PFN_vkGetInstanceProcAddr vkGetInstanceProcAddr =
         dynamicLoader.getProcAddress<PFN_vkGetInstanceProcAddr>("vkGetInstanceProcAddr");
     VULKAN_HPP_DEFAULT_DISPATCHER.init(vkGetInstanceProcAddr);
@@ -20,6 +28,10 @@ TEST(N1QuickStart, E3HelloTriangle)
     if (!instance.create()) qFatal("Failed to create Vulkan instance: %d", instance.errorCode());
     VULKAN_HPP_DEFAULT_DISPATCHER.init(instance.vkInstance());
 
-
     QLoggingCategory::setFilterRules(QStringLiteral("qt.vulkan=true"));
+    VulkanWindow vkWindow;
+    vkWindow.setVulkanInstance(&instance);
+    vkWindow.resize(1024, 768);
+    vkWindow.show();
+    return app.exec();
 }
