cb0187848fb496443c25b0f961d66bb458818b7f
gukazma <gukazma@proton.me>
2023-08-13 09:38:50 +0800

    ✨ feat: reconfigure shader

--- CMakeModules/ShaderCompile.cmake
+++ CMakeModules/ShaderCompile.cmake
@@ -1,7 +1,6 @@
 function(compile_shader SHADERS TARGET_NAME SHADER_INCLUDE_FOLDER GENERATED_DIR GLSLANG_BIN)
-    add_custom_target(${TARGET_NAME})
     set(working_dir "${CMAKE_CURRENT_SOURCE_DIR}")
-
+    add_custom_target(${TARGET_NAME})
     set(ALL_GENERATED_SPV_FILES "")
     set(ALL_GENERATED_CPP_FILES "")
 
@@ -11,6 +10,8 @@ function(compile_shader SHADERS TARGET_NAME SHADER_INCLUDE_FOLDER GENERATED_DIR 
 
     foreach(SHADER ${SHADERS})
     # Prepare a header name and a global variable for this shader
+        set_source_files_properties(${SHADER} PROPERTIES HEADER_FILE_ONLY TRUE)
+
         get_filename_component(SHADER_NAME ${SHADER} NAME)
         string(REPLACE "." "_" HEADER_NAME ${SHADER_NAME})
         string(TOUPPER ${HEADER_NAME} GLOBAL_SHADER_VAR)
@@ -38,8 +39,9 @@ function(compile_shader SHADERS TARGET_NAME SHADER_INCLUDE_FOLDER GENERATED_DIR 
         list(APPEND ALL_GENERATED_CPP_FILES ${CPP_FILE})
 
     endforeach()
-
-    
+    target_sources(${TARGET_NAME} PRIVATE
+    ${ALL_GENERATED_CPP_FILES}
+    )
     # add_custom_target(${TARGET_NAME}
     #     DEPENDS ${ALL_GENERATED_SPV_FILES} SOURCES ${SHADERS})
 
